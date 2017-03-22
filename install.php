1b1950420e4fc6ebb1570f21c233d6c7e7074757
MarkGaoGit <674517660@qq.com>
2017-03-22 16:58:35 +0800

    delete
    delete

--- install.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- *      [Powerlong] Copyright ? 2015-2016 Powerlong Real Estate Holdings Limited All rights reserved.
- *      This is NOT a freeware, use is subject to license terms
- *
- *      Author : gaokang
- *      tel:021-51759999
- */
-header('location:index.php?m=install');
